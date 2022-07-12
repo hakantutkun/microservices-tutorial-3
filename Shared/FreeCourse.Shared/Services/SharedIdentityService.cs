fb9020e946eb56069c94725e16c19d1f7b902ea5
hakan <hakan@DESKTOP-7IJG156.local>
2022-07-12 22:42:53 +0300

    58 - Shared kütüphanesine user id nin token üzerinden alınabilmesi için yardımcı bir servis eklendi.

---
+++ Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace FreeCourse.Shared.Services
+{
+    public class SharedIdentityService : ISharedIdentityService
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public string GetUserId
+        {
+            get { return _httpContextAccessor.HttpContext.User.FindFirst("sub").Value; }
+        }
+
+        public SharedIdentityService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor=httpContextAccessor;
+        }
+
+    }
+}
