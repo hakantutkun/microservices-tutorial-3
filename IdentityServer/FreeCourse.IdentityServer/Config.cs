25b5391f7363e3a2321816dce9f9880a87a2c242
hakan <hakan@DESKTOP-7IJG156.local>
2022-07-12 23:10:48 +0300

    62 - Basket mikroservis korunması için düzenlemeler tamamlandı.

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -14,7 +14,8 @@ namespace FreeCourse.IdentityServer
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
             new ApiResource("resource_catalog") {Scopes = {"catalog_fullpermission"}},
-            new ApiResource("photo_stock_catalog") {Scopes = {"photo_stock_fullpermission"}},
+            new ApiResource("resource_photo_stock") {Scopes = {"photo_stock_fullpermission"}},
+            new ApiResource("resource_basket") {Scopes = {"basket_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -35,6 +36,7 @@ namespace FreeCourse.IdentityServer
             {
                 new ApiScope("catalog_fullpermission","Catalog API icin full erisim"),
                 new ApiScope("photo_stock_fullpermission","Photo Stock API icin full erisim"),
+                new ApiScope("basket_fullpermission","Basket API icin full erisim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -58,6 +60,7 @@ namespace FreeCourse.IdentityServer
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                     AllowedScopes =
                     {
+                         "basket_fullpermission",
                          IdentityServerConstants.StandardScopes.Email,
                          IdentityServerConstants.StandardScopes.OpenId,
                          IdentityServerConstants.StandardScopes.Profile,
