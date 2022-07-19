b965be28e503f7b9596d64da343706eb14abe8a0
hakan <hakan@DESKTOP-7IJG156.local>
2022-07-20 00:16:13 +0300

    133 - CourseController index metodu eklendi.

---
+++ Frontends/FreeCourse.Web/Controllers/CoursesController.cs
@@ -0,0 +1,20 @@
+﻿using FreeCourse.Shared.Services;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    [Authorize]
+    public class CoursesController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _catalogService.GetAllCourseByUserIdAsync(_sharedIdentityService.GetUserId));
+        }
+    }
+}
