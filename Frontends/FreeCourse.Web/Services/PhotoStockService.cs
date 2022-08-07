3bb6b7a19f5fcd3c0391274551fae0953fd6a13a
hakan <hakan@DESKTOP-7IJG156.local>
2022-08-08 00:09:57 +0300

    143 - PhotoStockService 2

--- Frontends/FreeCourse.Web/Services/PhotoStockService.cs
+++ Frontends/FreeCourse.Web/Services/PhotoStockService.cs
@@ -1,20 +1,54 @@
 ﻿using FreeCourse.Web.Models.Catalogs.PhotoStocks;
 using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services
 {
     public class PhotoStockService : IPhotoStockService
     {
+        private readonly HttpClient _httpClient;
 
-
-        public Task<bool> DeletePhoto(string photoUrl)
+        public PhotoStockService(HttpClient httpClient)
         {
+            _httpClient=httpClient;
         }
 
-        public Task<PhotoViewModel> UploadPhoto(IFormFile photo)
+        public async Task<bool> DeletePhoto(string photoUrl)
         {
+            var response = await _httpClient.DeleteAsync($"photos?photoUrl={photoUrl}");
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<PhotoViewModel> UploadPhoto(IFormFile photo)
+        {
+            if (photo == null || photo.Length<=0)
+            {
+                return null;
+            }
+
+            var randomFileName = $"{Guid.NewGuid()}{Path.GetExtension(photo.FileName)}";
+
+            using var ms = new MemoryStream();
+
+            await photo.CopyToAsync(ms);
+
+            var multipartContent = new MultipartFormDataContent();
+            multipartContent.Add(new ByteArrayContent(ms.ToArray()), "photo", randomFileName);
+
+            var response = await _httpClient.PostAsync("photo", multipartContent);
+
+            if(response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            return await response.Content.ReadFromJsonAsync<PhotoViewModel>();
         }
     }
 }
