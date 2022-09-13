223321bc9ba3f987060fcfa2b1d834685c53b33b
hakan <hakan@DESKTOP-7IJG156.local>
2022-09-13 21:52:09 +0300

    161 - Discount servis abstract ve concrete implementasyonu yapıldı.

---
+++ Frontends/FreeCourse.Web/Services/DiscountService.cs
@@ -0,0 +1,13 @@
+﻿using FreeCourse.Web.Models.Models;
+using FreeCourse.Web.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        public Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+        }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IDiscountService.cs
@@ -0,0 +1,10 @@
+﻿using FreeCourse.Web.Models.Models;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IDiscountService
+    {
+        Task<DiscountViewModel> GetDiscount(string discountCode);
+    }
+}
