fc9cd944b7d38653a58ba8a8bf92b28fb570d0c3
hakan <hakan@DESKTOP-7IJG156.local>
2022-09-13 21:56:09 +0300

    162 - Basket servis GetDiscount metodu tamamlandı.

--- Frontends/FreeCourse.Web/Services/DiscountService.cs
+++ Frontends/FreeCourse.Web/Services/DiscountService.cs
@@ -1,13 +1,33 @@
-﻿using FreeCourse.Web.Models.Models;
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models.Models;
 using FreeCourse.Web.Services.Interfaces;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services
 {
     public class DiscountService : IDiscountService
     {
-        public Task<DiscountViewModel> GetDiscount(string discountCode)
+        private readonly HttpClient _httpClient;
+
+        public DiscountService(HttpClient httpClient)
         {
+            _httpClient=httpClient;
+        }
+
+        public async Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            var response = await _httpClient.GetAsync($"discounts/GetByCode/{discountCode}");
+
+            if(!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var discount = await response.Content.ReadFromJsonAsync<Response<DiscountViewModel>>();
+
+            return discount.Data;
         }
     }
 }
