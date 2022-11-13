8f04e3bb48724342efbcc96e3eca035fced62073
hakan <hakan@DESKTOP-7IJG156.local>
2022-11-13 18:42:44 +0300

    169 - Ödeme sistemine geçildi.

--- Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -1,4 +1,5 @@
-﻿using FreeCourse.Shared.ControllerBases;
+﻿using FreeCourse.Services.FakePayment.Models;
+using FreeCourse.Shared.ControllerBases;
 using FreeCourse.Shared.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,8 +11,9 @@ namespace FreeCourse.Services.FakePayment.Controllers
     public class FakePaymentsController : CustomBaseController
     {
         [HttpPost]
-        public IActionResult ReceivePayment()
+        public IActionResult ReceivePayment(PaymentDto paymentDto)
         {
+            // payment dto ile odeme islemi gerceklestir...
             return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
         }
     }
